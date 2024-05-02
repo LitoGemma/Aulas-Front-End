5585e04a64ada7648cb5fd71b4c7f9ca9ecc4313
Julio Brandao de Oliveira <julio53727236@aluno.ba.senac.br>
2024-05-02 20:59:54 -0300

    Aula12

--- Js/mudarCor.js
+++ Js/mudarCor.js
@@ -1,33 +1,48 @@
 var btnLight = document.querySelector(".btn-light");
 var btnDark = document.querySelector(".btn-dark");
 var body = document.querySelector("body");
-var section = document.querySelector("section");
-var navLinks = document.querySelector(".navbar a");
-
+var section = document.querySelectorAll("section");
+var navLinks = document.querySelectorAll(".navbar a");
+var footer = document.querySelector("footer");
 
 btnDark.addEventListener("click", function() {
-    body.style.background = 
+    body.style.background = "black";
     body.style.color = "white";
     btnDark.style.display = "none";
     btnLight.style.display = "block";
 
 
-navLinks.forEach(function(link) {
-    link.style.setProperty ("color", "white", "important")
+    navLinks.forEach(function(link) {
+        link.style.setProperty ("color", "white", "important");
+    });
+
+    section.forEach(function(sec) {
+        sec.style.backgroundColor = "rgb(241, 166, 213, 0.4)"
+    });
+
+    footer.style.backgroundColor = "rgb(241, 166, 213, 0.4)"
+  
+
 });
-})
-
-
 
 btnLight.addEventListener("click", function() {
-    body.style.background = 
+    body.style.background = "white";
     body.style.color = "black";
     btnDark.style.display = "block";
     btnLight.style.display = "none";
 
 
-navLinks.forEach (function(link) {
-    link.style.setProperty ("color", "black", "important")
+    navLinks.forEach (function(link) {
+        link.style.setProperty ("color", "black", "important");
+    });
+
+    section.forEach(function(sec) {
+        sec.style.backgroundColor = "rgb(248, 162, 149)"
+    }); 
+
+    footer.style.backgroundColor = "rgb(248, 162, 149)"
+   
+
+
 });
-})
 
